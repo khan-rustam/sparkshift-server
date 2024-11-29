63fef4ea97c4787278efe39296fd208daaf7d1cb
Rustam Khan <khanrustam2809@gmail.com>
2024-11-30 02:05:41 +0530

    Update server.js - changes the email image

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.post("/api/contact", async (req, res) => {
   <div
     style="background-color: #f4f4f4;  padding: 20px; align-items: center;  border-radius: 10px;   margin-bottom: 20px;" >
     <div style="color: #333333; font-size: 24px; font-weight: bold text-align: center align-items: center">
-      <img src='https://sjc04pap002files.storage.live.com/y4mvCGyech_gLBoEZHHxEK1c9CZleWU5HkFVb9vF-fZWNdpgZBONVn-Jkj26l-Q7A6RdMwMKI6xFrDlA1w5TaV2OOF9b2G-V7Ozfffjn0FD5jRx4eFsGi2gAxJ-InqpQ7b0tPNX6RbtfQFa6uhpfIRBKzjtnHK9aq9iXg9-Gvf2TqaWp99eJrhfdLGVGwzgZhiz5xmIishmB9REqAqW1E_7ee1IKX265kXTFCadPYG74UQ?encodeFailures=1&width=1188&height=1188' alt="Sparkshift" style="height: 50px; margin-bottom: 15px; filter: drop-shadow(0 4px 6px rgba(130, 87, 229, 0.2));">
+      <img src="https://sjc04pap002files.storage.live.com/y4mObO2InTZxxKcXmNAqPSGbVEU4fZk7glhLS_mEbZqt1uA_Q7Ee05NuqvaXUPnQy8_dVVksYtEJIubc4Bh4PWrT7IdyPcxTy_-O_cXNbshyfgObUV4yYMqgmTe70_l4qxwySim8GLQ3nH1pQNDuijWp1Y8csLfkNUoXOB12dXwc6ASltIoWfgSFVn2I2n7AavA6QU5k9KqhjBymVOdGIfXSHo0WrJ1IVSVdkCmrHwgygE?encodeFailures=1&width=880&height=219">
 
   </div>
   <div
